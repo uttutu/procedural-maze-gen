940fb1f0cc97e8d859afb08406826a32301f31e3
Utkarsh Singh <utkarsh.singh27800@gmail.com>
2020-10-18 02:59:39 +0530

    player interactions with surrounding added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,37 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
-    
+
+    public Rigidbody rb;
+    public float force = 50f;
+    public Transform player;
+    public Vector3 moveX;
+    public Vector3 moveZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("i"))
+        {
+            rb.AddForce(0, 0, force * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("j"))
+        {
+            rb.AddForce(-force * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("k"))
+        {
+            rb.AddForce(0, 0, -force * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("l"))
+        {
+            rb.AddForce(force * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
 }
